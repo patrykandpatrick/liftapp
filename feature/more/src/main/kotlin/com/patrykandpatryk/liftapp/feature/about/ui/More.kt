e39e8b07c21fe7df8b07178176af55fbc7a3b5fe
Patryk Michalik <contact@patrykmichalik.com>
2022-06-02 17:21:30 +0200

    Resolve issues related to code style

--- feature/more/src/main/kotlin/com/patrykandpatryk/liftapp/feature/about/ui/More.kt
+++ feature/more/src/main/kotlin/com/patrykandpatryk/liftapp/feature/about/ui/More.kt
@@ -2,9 +2,9 @@ package com.patrykandpatryk.liftapp.feature.about.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
