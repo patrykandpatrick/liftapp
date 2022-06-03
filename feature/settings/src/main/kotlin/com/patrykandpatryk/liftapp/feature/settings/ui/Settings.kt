ccc7aaa01eac3dde781fa1eb9829b26fec2d1f03
Patryk Michalik <contact@patrykmichalik.com>
2022-06-03 16:47:47 +0200

    Fix code formatting

--- feature/settings/src/main/kotlin/com/patrykandpatryk/liftapp/feature/settings/ui/Settings.kt
+++ feature/settings/src/main/kotlin/com/patrykandpatryk/liftapp/feature/settings/ui/Settings.kt
@@ -31,8 +31,6 @@ fun Settings(
         },
     ) { paddingValues ->
 
-        LazyColumn(contentPadding = paddingValues) {
-
-        }
+        LazyColumn(contentPadding = paddingValues) { }
     }
 }
