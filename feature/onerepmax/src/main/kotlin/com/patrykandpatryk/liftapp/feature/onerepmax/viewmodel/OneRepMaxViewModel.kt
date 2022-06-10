2c466447885e72ce8969c3516d4c229b356ebd6f
Patryk Michalik <contact@patrykmichalik.com>
2022-06-10 20:53:52 +0200

    1RM calculator: Store result history

--- feature/onerepmax/src/main/kotlin/com/patrykandpatryk/liftapp/feature/onerepmax/viewmodel/OneRepMaxViewModel.kt
+++ feature/onerepmax/src/main/kotlin/com/patrykandpatryk/liftapp/feature/onerepmax/viewmodel/OneRepMaxViewModel.kt
@@ -8,9 +8,13 @@ import com.patrykandpatryk.liftapp.core.viewmodel.SavedStateHandleViewModel
 import com.patrykandpatryk.liftapp.core.extension.smartToFloatOrNull
 import com.patrykandpatryk.liftapp.core.extension.smartToIntOrNull
 import com.patrykandpatryk.liftapp.domain.repository.PreferenceRepository
+import com.patrykandpatryk.liftapp.feature.onerepmax.model.HistoryEntryModel
 import com.patrykandpatryk.liftapp.feature.onerepmax.model.OneRepMaxUiState
 import com.patrykmichalik.opto.core.onEach
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,6 +33,8 @@ class OneRepMaxViewModel @Inject constructor(
         defaultValue = OneRepMaxUiState(),
     )
 
+    var historyUpdateJob: Job? = null
+
     init {
         preferenceRepository.massUnit.onEach(launchIn = viewModelScope) {
             oneRepMaxUiState = oneRepMaxUiState.copy(massUnit = it)
@@ -39,28 +45,62 @@ class OneRepMaxViewModel @Inject constructor(
 
         val reps = if (repsInput.isEmpty()) 0 else repsInput.smartToIntOrNull()
 
+        val oneRepMax =
+            if (reps != null) calculateOneRepMax(reps = reps, mass = oneRepMaxUiState.mass)
+            else oneRepMaxUiState.oneRepMax
+
         oneRepMaxUiState = oneRepMaxUiState.copy(
             repsInput = repsInput,
             reps = reps ?: 0,
             repsInputValid = reps != null,
-            oneRepMax = if (reps != null) {
-                calculateOneRepMax(reps = reps, mass = oneRepMaxUiState.mass)
-            } else oneRepMaxUiState.oneRepMax,
+            oneRepMax = oneRepMax,
         )
+
+        if (reps != null && oneRepMaxUiState.massInputValid) {
+            updateHistory(reps = reps, mass = oneRepMaxUiState.mass, oneRepMax = oneRepMax)
+        }
     }
 
     fun updateMassInput(massInput: String) {
 
         val mass = if (massInput.isEmpty()) 0f else massInput.smartToFloatOrNull()
 
+        val oneRepMax =
+            if (mass != null) calculateOneRepMax(reps = oneRepMaxUiState.reps, mass = mass)
+            else oneRepMaxUiState.oneRepMax
+
         oneRepMaxUiState = oneRepMaxUiState.copy(
             massInput = massInput,
             mass = mass ?: 0f,
             massInputValid = mass != null,
-            oneRepMax = if (mass != null) {
-                calculateOneRepMax(reps = oneRepMaxUiState.reps, mass = mass)
-            } else oneRepMaxUiState.oneRepMax,
+            oneRepMax = oneRepMax,
         )
+
+        if (mass != null && oneRepMaxUiState.repsInputValid) {
+            updateHistory(reps = oneRepMaxUiState.reps, mass = mass, oneRepMax = oneRepMax)
+        }
+    }
+
+    private fun updateHistory(
+        reps: Int,
+        mass: Float,
+        oneRepMax: Float,
+    ) {
+        historyUpdateJob?.cancel()
+        if (oneRepMax == 0f) return
+
+        historyUpdateJob = viewModelScope.launch {
+            delay(HISTORY_UPDATE_DELAY)
+            HistoryEntryModel(
+                reps = reps,
+                mass = mass,
+                oneRepMax = oneRepMax,
+            ).let {
+                if (oneRepMaxUiState.history.firstOrNull() == it) return@launch
+                oneRepMaxUiState = oneRepMaxUiState
+                    .copy(history = listOf(it) + oneRepMaxUiState.history)
+            }
+        }
     }
 
     private fun calculateOneRepMax(
@@ -76,5 +116,6 @@ class OneRepMaxViewModel @Inject constructor(
 
         private const val EPLEY_REP_COUNT_DIVISOR = 30f
         private const val ONE_REP_MAX_UI_STATE_KEY = "one_rep_max_ui_state"
+        private const val HISTORY_UPDATE_DELAY = 3000L
     }
 }
