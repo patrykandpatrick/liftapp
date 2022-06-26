07b452f70b352570b044bebed1e3d4b61d46cbaa
Patryk Goworowski <patryk.goworowski@angrynerds.co>
2022-06-26 10:04:43 +0200

    Add `SegmentedButton` and `Dimens`

---
+++ core/src/main/kotlin/com/patrykandpatryk/liftapp/core/ui/theme/Shapes.kt
@@ -0,0 +1,7 @@
+package com.patrykandpatryk.liftapp.core.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+@Suppress("MagicNumber")
+val PillShape = RoundedCornerShape(100.dp)
