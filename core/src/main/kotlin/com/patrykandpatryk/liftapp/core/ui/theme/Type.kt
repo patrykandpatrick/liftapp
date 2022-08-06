d22e06010e19a083ac61dc985e1753c34b27235a
Patryk Goworowski <patrykgoworowski@gmail.com>
2022-08-06 08:53:49 +0200

    Disable font padding for all `TextStyle`s in `Typography`

--- core/src/main/kotlin/com/patrykandpatryk/liftapp/core/ui/theme/Type.kt
+++ core/src/main/kotlin/com/patrykandpatryk/liftapp/core/ui/theme/Type.kt
@@ -1,5 +1,23 @@
+@file:OptIn(ExperimentalTextApi::class)
 package com.patrykandpatryk.liftapp.core.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
 
-val Typography = Typography()
+private val platformTextStyle = PlatformTextStyle(
+    includeFontPadding = false,
+)
+
+val Typography = Typography().run {
+    copy(
+     displayLarge = displayLarge.copy(platformStyle = platformTextStyle),
+     displayMedium = displayMedium.copy(platformStyle = platformTextStyle),
+     displaySmall = displaySmall.copy(platformStyle = platformTextStyle),
+     headlineLarge = headlineLarge.copy(platformStyle = platformTextStyle),
+     headlineMedium = headlineMedium.copy(platformStyle = platformTextStyle),
+     headlineSmall = headlineSmall.copy(platformStyle = platformTextStyle),
+     titleLarge = titleLarge.copy(platformStyle = platformTextStyle),
+     titleMedium = titleMedium.copy(platformStyle = platformTextStyle),
+    )
+}
