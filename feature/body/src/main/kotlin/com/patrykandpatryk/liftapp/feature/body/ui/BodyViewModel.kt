d53e47563c7f547e75b7aa85540862a0ae22345d
Patryk Goworowski <patrykgoworowski@gmail.com>
2022-08-28 11:23:08 +0200

    [`BodyViewModel`] Log exceptions on `getBodyItems` error

--- feature/body/src/main/kotlin/com/patrykandpatryk/liftapp/feature/body/ui/BodyViewModel.kt
+++ feature/body/src/main/kotlin/com/patrykandpatryk/liftapp/feature/body/ui/BodyViewModel.kt
@@ -3,18 +3,21 @@ package com.patrykandpatryk.liftapp.feature.body.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import javax.inject.Inject
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.plus
 
 @HiltViewModel
 class BodyViewModel @Inject constructor(
     getBodyItems: GetBodyItemsUseCase,
+    exceptionHandler: CoroutineExceptionHandler,
 ) : ViewModel() {
 
     val bodyItems = getBodyItems()
         .stateIn(
-            scope = viewModelScope,
+            scope = viewModelScope + exceptionHandler,
             started = SharingStarted.Eagerly,
             initialValue = emptyList(),
         )
