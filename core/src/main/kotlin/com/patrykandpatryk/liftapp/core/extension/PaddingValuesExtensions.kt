f5f8539a14f81efc41a0e3d4e5e1185d201533d9
Patryk Michalik <contact@patrykmichalik.com>
2022-10-16 18:09:26 +0200

    Annotate `PaddingValues#calculate*Padding` with `@ReadOnlyComposable`

--- core/src/main/kotlin/com/patrykandpatryk/liftapp/core/extension/PaddingValuesExtensions.kt
+++ core/src/main/kotlin/com/patrykandpatryk/liftapp/core/extension/PaddingValuesExtensions.kt
@@ -4,13 +4,16 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
 
 @Composable
+@ReadOnlyComposable
 fun PaddingValues.calculateStartPadding(): Dp =
     calculateStartPadding(LocalLayoutDirection.current)
 
 @Composable
+@ReadOnlyComposable
 fun PaddingValues.calculateEndPadding(): Dp =
     calculateEndPadding(LocalLayoutDirection.current)
