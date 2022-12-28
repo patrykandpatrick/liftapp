5b38ed7452ad109c2da2767e1bb89dbe90618f11
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2022-12-28 12:34:13 +0100

    Correct errors in `InsertDefaultRoutines`

--- functionality/database/src/main/kotlin/com/patrykandpatryk/liftapp/functionality/database/exercise/InsertDefaultRoutines.kt
+++ functionality/database/src/main/kotlin/com/patrykandpatryk/liftapp/functionality/database/exercise/InsertDefaultRoutines.kt
@@ -3,17 +3,23 @@ package com.patrykandpatryk.liftapp.functionality.database.exercise
 import com.patrykandpatryk.liftapp.functionality.database.routine.ExerciseWithRoutineEntity
 import com.patrykandpatryk.liftapp.functionality.database.routine.RoutineDao
 import com.patrykandpatryk.liftapp.functionality.database.routine.RoutineEntity
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 import kotlin.random.Random
 
 @Suppress("MagicNumber")
 class InsertDefaultRoutines @Inject constructor(
     private val routineDao: RoutineDao,
+    private val exerciseDao: ExerciseDao,
 ) {
 
     suspend operator fun invoke() {
 
         val random = Random.Default
+        val exerciseIDs = exerciseDao
+            .getAllExercises()
+            .first()
+            .map { exerciseEntity -> exerciseEntity.id }
 
         listOf(
             RoutineEntity(name = "Sample 1"),
@@ -23,11 +29,12 @@ class InsertDefaultRoutines @Inject constructor(
             RoutineEntity(name = "Sample 5"),
         ).map { routine ->
             val routineId = routineDao.upsert(routine)
+            val freeExerciseIDs = exerciseIDs.toMutableList()
             repeat(random.nextInt(6, 9)) {
                 routineDao.insert(
                     ExerciseWithRoutineEntity(
                         routineId = routineId,
-                        exerciseId = random.nextLong(104),
+                        exerciseId = freeExerciseIDs.removeAt(random.nextInt(freeExerciseIDs.size)),
                     ),
                 )
             }
