f7629cd28e2ca3f86403a5f60e9646f0eb8e8880
Patryk Goworowski <patrykgoworowski@gmail.com>
2023-01-29 12:22:58 +0100

    Rename `MuscleConverterTest` and add more Json convert tests

---
+++ functionality/database/src/test/kotlin/com/patrykandpatryk/liftapp/functionality/database/converter/JsonConvertersTest.kt
@@ -0,0 +1,57 @@
+package com.patrykandpatryk.liftapp.functionality.database.converter
+
+import com.patrykandpatryk.liftapp.domain.body.BodyValues
+import com.patrykandpatryk.liftapp.domain.di.DomainModule
+import com.patrykandpatryk.liftapp.domain.model.Name
+import com.patrykandpatryk.liftapp.domain.muscle.Muscle
+import com.patrykandpatryk.liftapp.domain.unit.MassUnit
+import com.patrykandpatryk.liftapp.functionality.database.string.ExerciseStringResource
+import org.junit.Test
+import kotlin.random.Random
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
+class JsonConvertersTest {
+
+    private val converter = JsonConverters(DomainModule.provideJson(emptySet()))
+
+    @Test
+    fun `Conversion of List of Muscles to String and back yields the same result`() {
+        val input = listOf(Muscle.Abs, Muscle.Chest, Muscle.Glutes)
+        val serializedInput = converter.toString(input)
+        val deserializedInput = converter.toMuscles(serializedInput)
+        assertContentEquals(input, deserializedInput)
+    }
+
+    @Test
+    fun `Conversion of Name#Raw to String and back yields the same result`() {
+        val input = Name.Raw("A raw name")
+        val serializedInput = converter.toString(input)
+        val deserializedInput = converter.toName(serializedInput)
+        assertEquals(input, deserializedInput)
+    }
+
+    @Test
+    fun `Conversion of Name#Resource to String and back yields the same result`() {
+        val input = Name.Resource(ExerciseStringResource.DumbbellLunges)
+        val serializedInput = converter.toString(input)
+        val deserializedInput = converter.toName(serializedInput)
+        assertEquals(input, deserializedInput)
+    }
+
+    @Test
+    fun `Conversion of BodyValues#Single to String and back yields the same result`() {
+        val input = BodyValues.Single(Random.nextFloat(), MassUnit.Kilograms)
+        val serializedInput = converter.toString(input)
+        val deserializedInput = converter.toBodyValues(serializedInput)
+        assertEquals(input, deserializedInput)
+    }
+
+    @Test
+    fun `Conversion of BodyValues#Double to String and back yields the same result`() {
+        val input = BodyValues.Double(Random.nextFloat(), Random.nextFloat(), MassUnit.Kilograms)
+        val serializedInput = converter.toString(input)
+        val deserializedInput = converter.toBodyValues(serializedInput)
+        assertEquals(input, deserializedInput)
+    }
+}
