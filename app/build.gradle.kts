72d469e8e1f49b8b7f83b94561d54d122337ba0f
Patryk Goworowski <patrykgoworowski@gmail.com>
2024-10-06 17:51:22 +0200

    convert `:app` `build.gradle` to Kotlin. Make it automatically implement all functionality modules

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+apply {
+    from("$rootDir/gradle/ui-module-base.gradle")
+}
+
+plugins {
+    alias(libs.plugins.application)
+}
+
+android {
+    namespace = "pl.patrykgoworowski.mintlift"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+
+        debug {
+            applicationIdSuffix = ".dev"
+            isDebuggable = true
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":data"))
+    implementation(project(":navigation"))
+    implementation(project(":feature:home"))
+    implementation(project(":feature:home:exercises")) // TODO avoid
+    implementation(project(":feature:settings"))
+    implementation(project(":feature:about"))
+    implementation(project(":feature:onerepmax"))
+    implementation(project(":feature:newexercise"))
+    implementation(project(":feature:exercisedetails"))
+    implementation(project(":feature:newbodymeasuremententry"))
+    implementation(project(":feature:bodymeasurementdetails"))
+    implementation(project(":feature:routine"))
+    implementation(project(":feature:newroutine"))
+    implementation(project(":feature:exercisegoal"))
+    project(":functionality") {
+        subprojects.forEach(::implementation)
+    }
+}
